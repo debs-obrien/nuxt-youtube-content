8b83791332ab4f989978818998440425588ce4a4
Debbie O'Brien <debs_obrien@yahoo.co.uk>
2020-10-13 18:07:42 +0200

    feat: refactor fetch to use in components

--- components/ProductCard.vue
+++ components/ProductCard.vue
@@ -1,34 +1,58 @@
 <template>
-  <div class="md:flex">
-    <div class="md:flex-shrink-0">
-      <img
-        class="rounded-lg md:w-56"
-        :src="product.image"
-        :alt="product.title"
-      />
-    </div>
-    <div class="mt-4 md:mt-0 md:ml-6">
-      <div class="uppercase tracking-wide text-sm text-indigo-600 font-bold">
-        {{ product.continent }}
+  <div>
+    <button
+      class="text-sm bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-2 rounded-full mb-4"
+      @click="$fetch"
+    >
+      Refresh
+    </button>
+    <p v-if="$fetchState.pending">Fetching mountains...</p>
+    <p v-else-if="$fetchState.error">An error occured :(</p>
+    <div
+      v-for="mountain in mountains"
+      v-else
+      :key="mountain.title"
+      class="mb-4 flex felx-col text-left md:flex"
+    >
+      <div class="md:flex-shrink-0">
+        <img
+          class="rounded-lg md:w-56"
+          :src="mountain.image"
+          :alt="mountain.title"
+        />
       </div>
-      <NuxtLink
-        :to="product.slug"
-        class="block mt-1 text-lg leading-tight font-semibold text-gray-900 hover:underline"
-        >{{ product.title }}
-      </NuxtLink>
-      <p class="mt-2 text-gray-600">
-        {{ product.description }}
-      </p>
+      <div class="mt-4 md:mt-0 md:ml-6">
+        <div class="uppercase tracking-wide text-sm text-indigo-600 font-bold">
+          {{ mountain.continent }}
+        </div>
+        <NuxtLink
+          :to="mountain.slug"
+          class="block mt-1 text-lg leading-tight font-semibold text-gray-900 hover:underline"
+          >{{ mountain.title }}
+        </NuxtLink>
+        <p class="mt-2 text-gray-600">
+          {{ mountain.description }}
+        </p>
+      </div>
     </div>
   </div>
 </template>
 <script>
 export default {
-  props: {
-    product: {
-      type: Object,
-      default: () => {}
+  async fetch() {
+    this.mountains = await fetch(
+      'https://api.nuxtjs.dev/mountains'
+    ).then((res) => res.json())
+  },
+  data() {
+    return {
+      mountains: []
     }
   }
 }
 </script>
+<style lang="postcss">
+img {
+  height: 120px;
+}
+</style>
--- pages/products.vue
+++ pages/products.vue
@@ -4,39 +4,10 @@
     <NuxtLink to="/" class="text-left text-grey-600 hover:underline">
       Home
     </NuxtLink>
-    <button
-      class="text-sm bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-2 rounded-full mb-4"
-      @click="$fetch"
-    >
-      Refresh
-    </button>
-    <p v-if="$fetchState.pending">Fetching mountains...</p>
-    <p v-else-if="$fetchState.error">An error occured :(</p>
-    <ProductCard
-      v-for="mountain in mountains"
-      v-else
-      :key="mountain.title"
-      :product="mountain"
-      class="mb-4 flex felx-col text-left"
-    />
+
+    <ProductCard />
   </div>
 </template>
 <script>
-export default {
-  async fetch() {
-    this.mountains = await fetch(
-      'https://api.nuxtjs.dev/mountains'
-    ).then((res) => res.json())
-  },
-  data() {
-    return {
-      mountains: []
-    }
-  }
-}
+export default {}
 </script>
-<style lang="postcss">
-img {
-  height: 120px;
-}
-</style>
