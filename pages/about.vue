dede82cb0ab6e0b1f21e3c9cead80db84b5f55cf
Debbie O'Brien <debs_obrien@yahoo.co.uk>
2020-10-13 11:00:12 +0200

    feat: content from first few videos

---
+++ pages/about.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="container">
+    <div>
+      <h1 class="title">About</h1>
+      <p v-if="$nuxt.isOffline">oops you are offline 😱</p>
+      <p v-if="$nuxt.isOnline">yeahhhh 😍</p>
+      <NuxtLink to="/" class="button--green">Home</NuxtLink>
+    </div>
+  </div>
+</template>
---
+++ pages/posts/_id.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>{{ post.title }}</h1>
+    <p>{{ post.description }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData({ params, $axios }) {
+    const post = await $axios.$get(`https://api.nuxtjs.dev/posts/${params.id}`)
+    return { post }
+  },
+}
+</script>
